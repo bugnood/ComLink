c7ca7e59b87fd9bd213eb5842c1b13ded3f13aa2
daisuke.abe <commityneo@gmail.com>
2023-09-10 09:30:26 +0900

    Initial Commit

---
+++ ComLink/ComLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComLinkApp.swift
+//  ComLink
+//
+//  Created by 阿部大輔 on 2023/09/09.
+//
+
+import SwiftUI
+
+@main
+struct ComLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComLink/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ComLink
+//
+//  Created by 阿部大輔 on 2023/09/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+            Text("初めてのアプリ開発！")
+            Text("楽しみです！")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
